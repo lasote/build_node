87e4811d74846b15bf7950eac029a9f5de05c050
Luis Martinez de Bartolome <lasote@gmail.com>
2019-06-14 22:58:18 +0200

    Get win prepared

---
+++ run.bat
@@ -0,0 +1,6 @@
+pip uninstall -y conan_package_tools
+pip install conan --upgrade
+pip install --index-url https://test.pypi.org/simple/ conan==1.17.0.dev1560518880
+git clone https://github.com/lasote/ci-conan-flows.git ci
+pip install -r ci/requirements.txt
+PYTHONPATH=$(pwd)/ci python ci/conan_ci/launch_single_job.py
