567e22cfa6465897c571d48f0d6e41d2f4809341
Luis Martinez de Bartolome <lasote@gmail.com>
2019-06-14 15:35:24 +0200

    run script

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+pip install --index-url https://test.pypi.org/simple/ conan==1.17.0.dev1560518880
+git clone git@github.com:lasote/ci-conan-flows.git ci
+pip install -r ci/requirements.txt
+PYTHONPATH=$(pwd)/ci python ci/conan_ci/launch_single_job.py
