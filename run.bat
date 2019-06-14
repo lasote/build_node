6512e6027c7c156982cf6eb8a452bdaa79eff182
Luis Martinez de Bartolome <lasote@gmail.com>
2019-06-14 23:37:05 +0200

    Try now

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
-pip uninstall -y conan_package_tools
-pip install conan --upgrade
-pip install --index-url https://test.pypi.org/simple/ conan==1.17.0.dev1560518880
+$PIP uninstall -y conan_package_tools
+$PIP install conan --upgrade
+$PIP install --index-url https://test.pypi.org/simple/ conan==1.17.0.dev1560518880
 git clone https://github.com/lasote/ci-conan-flows.git ci
-pip install -r ci/requirements.txt
+$PIP install -r ci/requirements.txt
 PYTHONPATH=$(pwd)/ci python ci/conan_ci/launch_single_job.py
