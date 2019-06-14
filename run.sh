f595da2344d16e9695fa718faae503f38562dc6a
Luis Martinez de Bartolome <lasote@gmail.com>
2019-06-14 16:18:51 +0200

    Clone differently

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ set -e
 pip uninstall -y conan_package_tools
 pip install conan --upgrade
 pip install --index-url https://test.pypi.org/simple/ conan==1.17.0.dev1560518880
-git clone git@github.com:lasote/ci-conan-flows.git ci
+git clone https://github.com/lasote/ci-conan-flows.git ci
 pip install -r ci/requirements.txt
 PYTHONPATH=$(pwd)/ci python ci/conan_ci/launch_single_job.py
