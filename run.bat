c1a73e33701621be0b5a1bd075ce7c37cdcce4ae
Luis Martinez de Bartolome <lasote@gmail.com>
2019-06-15 11:39:18 +0200

    Windows fail on error

--- run.bat
+++ run.bat
@@ -2,9 +2,14 @@ if not exist "C:/Python37/" choco install python --version 3.7.2
 set PATH=c:/python37/Scripts;c:/python37/;%PATH%
 set PYTHON=C:/Python37/python.exe
 set PIP=C:/Python37/Scripts/pip.exe
-%PIP% uninstall -y conan_package_tools
-%PIP% install conan --upgrade
-%PIP% install --index-url https://test.pypi.org/simple/ conan==1.17.0.dev1560518880
-git clone https://github.com/lasote/ci-conan-flows.git ci
-%PIP% install -r ci/requirements.txt
-set PYTHONPATH=%cd%/ci python ci/conan_ci/launch_single_job.py
+%PIP% uninstall -y conan_package_tools || goto :error
+%PIP% install conan --upgrade || goto :error
+%PIP% install --index-url https://test.pypi.org/simple/ conan==1.17.0.dev1560518880 || goto :error
+git clone https://github.com/lasote/ci-conan-flows.git ci || goto :error
+%PIP% install -r ci/requirements.txt || goto :error
+set PYTHONPATH=%cd%\ci
+python ci/conan_ci/launch_single_job.py || goto :error
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
