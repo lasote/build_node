db24edc7e8b94527785aaf9fee550d9629560f73
Luis Martinez de Bartolome <lasote@gmail.com>
2019-07-03 13:48:29 +0200

    all docker

--- run.sh
+++ run.sh
@@ -2,8 +2,6 @@
 set -e
 # pip install conan --upgrade
 # pip install --index-url https://test.pypi.org/simple/ conan==1.17.0.dev1560518880
-git clone https://github.com/memsharded/conan.git --branch feature/lockfiles conan
-pip install -e conan
 git clone https://github.com/lasote/ci-conan-flows.git ci
 pip install -r ci/requirements.txt
 PYTHONPATH=$(pwd)/ci python ci/conan_ci/launch_single_job.py
