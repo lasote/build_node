ff84c71fe2ef2885b91e4de64a5f8538b7a363b6
Luis Martinez de Bartolome <lasote@gmail.com>
2019-07-03 13:22:09 +0200

    Install conan from dev

--- run.bat
+++ run.bat
@@ -4,9 +4,10 @@ set PATH=c:/python37/Scripts;c:/python37/;%PATH%
 set PYTHON=C:/Python37/python.exe
 set PIP=C:/Python37/Scripts/pip.exe
 dir C:/Python37/Scripts/
-%PIP% uninstall -y conan_package_tools
-%PIP% install conan --upgrade || goto :error
-%PIP% install --index-url https://test.pypi.org/simple/ conan==1.17.0.dev1560518880 || goto :error
+REM %PIP% install conan --upgrade || goto :error
+REM %PIP% install --index-url https://test.pypi.org/simple/ conan==1.17.0.dev1560518880 || goto :error
+git clone https://github.com/memsharded/conan.git --branch feature/lockfiles conan || goto :error
+%PIP% install -e conan || goto :error
 git clone https://github.com/lasote/ci-conan-flows.git ci || goto :error
 %PIP% install -r ci/requirements.txt || goto :error
 set PYTHONPATH=%cd%\ci
--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
 pip uninstall -y conan_package_tools
-pip install conan --upgrade
-pip install --index-url https://test.pypi.org/simple/ conan==1.17.0.dev1560518880
+# pip install conan --upgrade
+# pip install --index-url https://test.pypi.org/simple/ conan==1.17.0.dev1560518880
+git clone https://github.com/memsharded/conan.git --branch feature/lockfiles conan
+pip install -e conan
 git clone https://github.com/lasote/ci-conan-flows.git ci
 pip install -r ci/requirements.txt
 PYTHONPATH=$(pwd)/ci python ci/conan_ci/launch_single_job.py
