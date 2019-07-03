48263444a2c74e304897844bc0a8890d7d6afbbe
Luis Martinez de Bartolome <lasote@gmail.com>
2019-07-03 13:43:53 +0200

    run

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
-pip uninstall -y conan_package_tools
 # pip install conan --upgrade
 # pip install --index-url https://test.pypi.org/simple/ conan==1.17.0.dev1560518880
 git clone https://github.com/memsharded/conan.git --branch feature/lockfiles conan
